eacc6d2c15b0858094f83c43c65397dc220375f8
Syn3ed <lol-uuu@bk.ru>
2024-06-04 12:46:48 +0700

    modified:   src/components/FormApplication/createForm.jsx 	modified:   src/components/UI/CustomTextArea.jsx

--- src/components/UI/CustomTextArea.jsx
+++ src/components/UI/CustomTextArea.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
 
 const CustomTextArea = (props) => {
     const [text, setText] = useState(props.text || "");
@@ -8,54 +9,109 @@ const CustomTextArea = (props) => {
         setText(props.text || "");
     }, [props.text]);
 
-    const handleTextChange = (event) => {
-        const newText = event.target.value;
+    const handleTextChange = (event, value) => {
+        const newText = value !== undefined ? value : event.target.value;
         setText(newText);
         props.onTextChange(newText);
     };
 
+    const renderOption = (props, option, { selected }) => (
+        <li {...props} style={{ borderBottom: '2px solid #ddd' }}>
+            {option}
+        </li>
+    );
+
     return (
         <div className="CustomTextArea">
-            <TextField
-                className="custom-textarea"
-                label={props.label}
-                placeholder={props.placeholder}
-                multiline
-                variant="standard"
-                onChange={handleTextChange}
-                value={text}
-                InputProps={{
-                    disableUnderline: true,
-                    style: {
-                        fontSize: '16px',
-                        color: '#282B2F',
-                        fontWeight: 600,
-                        lineHeight: '24px',
-                        textAlign: 'left',
-                        font: 'Onest'
-                    }
-                }}
-                InputLabelProps={{
-                    style: {
-                        fontSize: '20px',
-                        color: '#637175',
-                        fontWeight: 600,
-                        lineHeight: '24px',
-                        textAlign: 'left',
-                        font: 'Onest'
-                    },
-                    placeholder: {
-                        fontSize: '20px',
-                        color: 'gray',
-                        fontWeight: 600,
-                        lineHeight: '24px',
-                        textAlign: 'left',
-                        font: 'Onest'
-                    }
-                }}
-            />
+            {props.options ? (
+                <Autocomplete
+                    options={props.options}
+                    inputValue={text}
+                    onInputChange={handleTextChange}
+                    renderOption={renderOption}
+                    renderInput={(params) => (
+                        <TextField
+                            {...params}
+                            label={props.label}
+                            placeholder={props.placeholder}
+                            multiline
+                            variant="standard"
+                            InputProps={{
+                                ...params.InputProps,
+                                disableUnderline: true,
+                                style: {
+                                    fontSize: '16px',
+                                    color: '#282B2F',
+                                    fontWeight: 600,
+                                    lineHeight: '24px',
+                                    textAlign: 'left',
+                                    font: 'Onest'
+                                }
+                            }}
+                            InputLabelProps={{
+                                style: {
+                                    fontSize: '20px',
+                                    color: '#637175',
+                                    fontWeight: 600,
+                                    lineHeight: '24px',
+                                    textAlign: 'left',
+                                    font: 'Onest'
+                                },
+                                placeholder: {
+                                    fontSize: '20px',
+                                    color: 'gray',
+                                    fontWeight: 600,
+                                    lineHeight: '24px',
+                                    textAlign: 'left',
+                                    font: 'Onest'
+                                }
+                            }}
+                        />
+                    )}
+                />
+            ) : (
+                <TextField
+                    className="custom-textarea"
+                    label={props.label}
+                    placeholder={props.placeholder}
+                    multiline
+                    variant="standard"
+                    onChange={handleTextChange}
+                    value={text}
+                    InputProps={{
+                        disableUnderline: true,
+                        style: {
+                            fontSize: '16px',
+                            color: '#282B2F',
+                            fontWeight: 600,
+                            lineHeight: '24px',
+                            textAlign: 'left',
+                            font: 'Onest'
+                        }
+                    }}
+                    InputLabelProps={{
+                        style: {
+                            fontSize: '20px',
+                            color: '#637175',
+                            fontWeight: 600,
+                            lineHeight: '24px',
+                            textAlign: 'left',
+                            font: 'Onest'
+                        },
+                        placeholder: {
+                            fontSize: '20px',
+                            color: 'gray',
+                            fontWeight: 600,
+                            lineHeight: '24px',
+                            textAlign: 'left',
+                            font: 'Onest'
+                        }
+                    }}
+                />
+            )}
         </div>
     );
 }
 
 export default CustomTextArea;
+
